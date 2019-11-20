6dd7cadfa99ed8480b7e2b5b70561cc1a1e4f6e2
Oscar Azpeitia <oscar@ecocode.de>
2019-11-20 16:47:35 +0100

    Control structures

---
+++ control_structures.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# if/else/elif
+age = 18
+
+if age >= 18:
+	print 'is an adult'
+elif age >= 12:
+	print 'is a young adult'
+elif age >= 3:
+	print 'child'
+else:
+	print 'baby'
+
+# ternary
+if age >= 21:
+	old_enough = True
+else:
+	old_enough = False
+
+old_enough = True if age >= 21 else False
+
+# while
+while age < 20:
+	print 'not old enough, current age is %d' % age
+	age += 1
