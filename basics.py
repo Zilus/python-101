e14f76ac3b9c9c62458969cd912936bc484cc453
Oscar Azpeitia <oscar@ecocode.de>
2019-11-20 15:17:58 +0100

    basic syntax

---
+++ basics.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+if True:
+    print 'hello'
+    print 'foo'
+
+#coment 
+
+"""
+comment 
+on 
+multiple
+lines
+"""    
+
+name = 'Oscar'
+num1 = 2; num2 = 6
+num3 = num1
+num4 = num1 + num2
+
+print num1
+print num2
+print num3
+print num4
+
+#We can use nuew lines by adding a backslash
+long_name = \
+    "Something " + \
+    "super " + "long!"
+
+print long_name
+
+#add a comma at the end
+print 'foobar no new line',
+print 'this is on the same line'
