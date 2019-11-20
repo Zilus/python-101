ebfa24091747c4ffc963fafa7a1222d721c1d177
Oscar Azpeitia <oscar@ecocode.de>
2019-11-20 16:42:27 +0100

    strings

---
+++ strings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+#single line
+text = 'hello'
+
+#multi line
+mutli_line = """Line 1""" \
+    """ line 2 """
+
+#substrings
+name = 'Oscar Azpeitia'
+first_name = name[0:5]
+
+print first_name
+
+#integer
+num1 = 5
+
+#float
+dec1 = 3.14
+
+# convertion to string
+to_string = str(num1)
+
+# convertion to integer/float
+num_string = '2'
+to_int = int(num_string)
+to_float = float(num_string)
+
+print to_int
+print to_float
