8e617ca08105d979ddae4890f8d62484b83c24ad
Oscar Azpeitia <oscar@ecocode.de>
2019-11-20 17:15:44 +0100

    classes

---
+++ use_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# inclusion
+from person import Person
+
+p = Person('Brian', 37)
+p.say_name()
+p.say_age()
+p.have_birthday()
+p.say_age()
+p.have_birthday()
+p.say_age()
+p.have_birthday()
+p.say_age()
+p.have_birthday()
+p.say_age()
